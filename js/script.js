c94cc59001fc5004c42458f2dc442086ed1abfb7
Mignon Van Der Voort <mignonvdvoort@gmail.com>
2024-03-10 14:17:53 +0100

    prettier

--- js/script.js
+++ js/script.js
@@ -1,26 +1,31 @@
-let tasks = [{ 
-        taskName: "Groceries", 
-        image: "images/vegetables-752153_1280.jpg", 
-        description: "Buy eggs, tomatoes, feta, milk, bread, rice and potatoes for the week.", 
-        importance: 0 
-      },{
-        taskName: "Doctor's appointment", 
-        image: "images/medical-563427_1280.jpg", 
-        description: "Make appointment with eye doctor for next week.", 
-        importance: 0
-      },{
-        taskName: "Drop off clothes", 
-        image: "images/fashion-1283863_1280.jpg", 
-        description: "Gather and drop off your old clothes at the local charity.", 
-        importance: 0
-      }]
+let tasks = [
+  {
+    taskName: "Groceries",
+    image: "images/vegetables-752153_1280.jpg",
+    description:
+      "Buy eggs, tomatoes, feta, milk, bread, rice and potatoes for the week.",
+    importance: 0,
+  },
+  {
+    taskName: "Doctor's appointment",
+    image: "images/medical-563427_1280.jpg",
+    description: "Make appointment with eye doctor for next week.",
+    importance: 0,
+  },
+  {
+    taskName: "Drop off clothes",
+    image: "images/fashion-1283863_1280.jpg",
+    description: "Gather and drop off your old clothes at the local charity.",
+    importance: 0,
+  },
+];
 
-let cards = document.getElementById("cards")
+let cards = document.getElementById("cards");
 
 function createCards() {
-    let color = "success"           /* ???????????????????? */
+  let color = "success"; /* ???????????????????? */
 
-    tasks.forEach(element => {
+  tasks.forEach((element) => {
     cards.innerHTML += `
     <div class="col-sm">
         <div class="card" style="width: 18rem;">
@@ -37,58 +42,56 @@ function createCards() {
             </div>
         </div>
     </div>
-    `
-    let upBtns = document.querySelectorAll(".up")
+    `;
+    let upBtns = document.querySelectorAll(".up");
 
-    upBtns.forEach((element,i) => {
-        
-        element.addEventListener("click", function () {
-            if (tasks[i].importance == 5) {              
-            } else {
-                tasks[i].importance++ 
-                document.querySelectorAll(".priority")[i].innerHTML = `Priority level: ${tasks[i].importance}`
-            }          
-        })
+    upBtns.forEach((element, i) => {
+      element.addEventListener("click", function () {
+        if (tasks[i].importance == 5) {
+        } else {
+          tasks[i].importance++;
+          document.querySelectorAll(".priority")[
+            i
+          ].innerHTML = `Priority level: ${tasks[i].importance}`;
+        }
+      });
     });
 
-    let lowerBtns = document.querySelectorAll(".lower")
+    let lowerBtns = document.querySelectorAll(".lower");
 
-    lowerBtns.forEach((element,i) => {
-        element.addEventListener("click", function () {
-            if (tasks[i].importance == 0) {               
-            } else {
-                tasks[i].importance-- 
-                document.querySelectorAll(".priority")[i].innerHTML = `Priority level: ${tasks[i].importance}`
-            }          
-        })
+    lowerBtns.forEach((element, i) => {
+      element.addEventListener("click", function () {
+        if (tasks[i].importance == 0) {
+        } else {
+          tasks[i].importance--;
+          document.querySelectorAll(".priority")[
+            i
+          ].innerHTML = `Priority level: ${tasks[i].importance}`;
+        }
+      });
     });
 
-
-
-    document.querySelector(".sort").addEventListener("click", function () {          /* ??????????????? */
-    tasks.sort(function(a, b){return a.importance - b.importance});
-    })
-
-   
-   
-   
-    let priorityBtns = document.querySelectorAll(".priorityBtn")                   /*  ?????????????????
- */
-    priorityBtns.forEach((element,i) => {
-        element.addEventListener("click", function () {
-            if (tasks[i].importance == 0 || 1) {
-                color = "success"
-            } else if (tasks[i].importance == 2 || 3){
-                color = "warning"
-            } else{
-                color = "danger"
-            }
-        })
+    /*  ???????????? */
+    document.querySelector(".sort").addEventListener("click", function () {
+      tasks.sort(function (a, b) {
+        return a.importance - b.importance;
+      });
     });
+    /* ?????????????? */
+    let priorityBtns = document.querySelectorAll(".priorityBtn");
 
-});
+    priorityBtns.forEach((element, i) => {
+      element.addEventListener("click", function () {
+        if (tasks[i].importance == 0 || 1) {
+          color = "success";
+        } else if (tasks[i].importance == 2 || 3) {
+          color = "warning";
+        } else {
+          color = "danger";
+        }
+      });
+    });
+  });
 }
 
-createCards()
-
-
+createCards();
